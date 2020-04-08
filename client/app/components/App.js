ddb09f35a568b985df6d6db8e1a4df9f4c697e8c
isabelle-wagenvoord <isabellewagenvoord@gmail.com>
2020-04-07 22:51:53 -0700

    add socket io and express. make express server

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+let P2P = require('socket.io-p2p');
+let io = require('socket.io-client');
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+    }
+    componentDidMount() {
+        let socket = io();
+        let p2p = new P2P(socket);
+        p2p.on('peer-msg', function (data) {
+            console.log('From a peer %s', data);
+        });
     }
     render() {
         return <h1>Hello world</h1>
